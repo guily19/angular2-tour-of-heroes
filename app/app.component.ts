958bf0cae0611f15586c4a5896b59491494d42c9
guillem.casanova <guillemcasanova@gmail.com>
2016-10-18 13:31:23 +0100

    Add selectedHero listener

--- app/app.component.ts
+++ app/app.component.ts
@@ -26,7 +26,7 @@ const HEROES: Hero[] = [
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+    <li *ngFor="let hero of heroes" (click)="onSelect(hero)>
     <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
@@ -82,7 +82,16 @@ const HEROES: Hero[] = [
 `]
 
 })
+
+
+
 export class AppComponent {
   title = 'Tour of Heroes';
+  selectedHero: Hero;
   heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
