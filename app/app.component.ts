9a744d9fb4371845593be2be7ba8448f851918da
guily19 <guillemcasanova@gmail.com>
2017-01-03 10:24:08 +0100

    Add promise to hero service and implement ngOnInit function

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
@@ -65,15 +65,19 @@ import { HeroService } from './hero.service';
     }
   `]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
   constructor(private heroService: HeroService) { }
 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
 
